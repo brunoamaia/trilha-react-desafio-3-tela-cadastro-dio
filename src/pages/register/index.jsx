8a63302b79a3b13a7711b33c5a766b120d2b6554
brunoamaia <brunoamaia94@gmail.com>
2023-02-27 16:19:29 -0300

    Criação da tela de cadastro

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,70 @@
+import { useNavigate } from 'react-router-dom';
+import { MdEmail, MdLock, MdPerson } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { api } from '../../services/api';
+
+import { useForm } from 'react-hook-form';
+
+import { AlreadyAccountText, Container, Title, Column, TitleLogin, SubtitleLogin, InfoText, LoginText, Row, NavigateRow, Wrapper } from './styles';
+
+const Register = () => {
+	const navigate = useNavigate()
+	const handleChangeLogin = () => navigate('/login');
+	const { control, handleSubmit, formState: { errors } } = useForm({
+		reValidateMode: 'onChange',
+		mode: 'onChange',
+	});
+
+	const onSubmit = async formData => {
+		try {
+			const { data } = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+
+			if (data.length && data[0].id) {
+				navigate('/feed')
+				return
+			}
+
+			alert('Usuário ou senha inválido')
+		} catch (e) {
+			//TODO: HOUVE UM ERRO
+		}
+	};
+
+	console.log('errors', errors);
+
+	return (<>
+		<Header />
+		<Container>
+			<Column>
+				<Title>{ 'A plataforma para você aprender com experts, dominar as principais tecnologias ' }
+					{ 'e entrar mais rápido nas empresas mais desejadas.' }</Title>
+			</Column>
+			<Column>
+				<Wrapper>
+					<TitleLogin>{ 'Comece agora grátis' }</TitleLogin>
+					<SubtitleLogin>{ 'Crie sua conta e make the change._' }</SubtitleLogin>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<Input type={'text'} placeholder={'Nome completo'} leftIcon={<MdPerson />} name={'nome'} control={control} />
+						{ errors.senha && <span>{ 'Senha é obrigatório' }</span> }
+						<Input placeholder={'E-mail'} leftIcon={<MdEmail />} name={'email'} control={control} />
+						{ errors.email && <span>{ 'E-mail é obrigatório' }</span> }
+						<Input type={'password'} placeholder={'Senha'} leftIcon={<MdLock />} name={'senha'} control={control} />
+						{ errors.senha && <span>{ 'Senha é obrigatório' }</span> }
+						<Button title={'Criar minha conta'} variant={'secondary'} type={'submit'} />
+					</form>
+					<Row>
+						<InfoText>{ 'Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.' }</InfoText>
+					</Row>
+					<NavigateRow>
+						<AlreadyAccountText>{ 'Já tenho conta.' }</AlreadyAccountText>
+						<LoginText onClick={handleChangeLogin}>{ 'Fazer login' }</LoginText>
+					</NavigateRow>
+				</Wrapper>
+			</Column>
+		</Container>
+	</>)
+}
+
+export { Register }
